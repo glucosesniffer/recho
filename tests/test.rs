89df9c69ef95354aed6b5135f8ccde3bc8b83832
ahmad <ahmadsaeed3290@gmail.com>
2026-02-20 04:19:04 +0500

    chore: add tests for other cases

--- tests/test.rs
+++ tests/test.rs
@@ -1,24 +1,42 @@
 use std::fs;
-
 use predicates::prelude::*;
 use assert_cmd::Command;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args(){
-    let mut cmd = Command::cargo_bin("recho").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("recho")?;
     cmd.assert().failure().stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn runs(){
-    let mut cmd = Command::cargo_bin("recho").unwrap();
+fn program_runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("recho")?;
     cmd.arg("hello").assert().success();
+    Ok(())
+}
+
+fn runs(args: &[&str], file: &str) -> TestResult {
+    let expected_file = fs::read_to_string(file)?;
+    Command::cargo_bin("recho")?.args(args).assert().success().stdout(expected_file);
+    Ok(())
 }
 
 #[test]
-fn hello(){
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("recho").unwrap();
-    cmd.arg("Hello there").assert().success().stdout(expected);
+fn hello1() -> TestResult{
+    runs(&["Hello there"], "tests/expected/hello1.txt")
+} 
+#[test]
+fn hello2() -> TestResult{
+    runs(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+#[test]
+fn hello1n() -> TestResult{
+    runs(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+#[test]
+fn hello2n() -> TestResult{
+    runs(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
