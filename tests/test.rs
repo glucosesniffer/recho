bb4a3b261e4f6e76fef20372660f396216a3d521
ahmad <ahmadsaeed3290@gmail.com>
2026-02-20 03:04:47 +0500

    chore: add tests

---
+++ tests/test.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+use predicates::prelude::*;
+use assert_cmd::Command;
+
+#[test]
+fn dies_no_args(){
+    let mut cmd = Command::cargo_bin("recho").unwrap();
+    cmd.assert().failure().stderr(predicate::str::contains("USAGE"));
+}
+
+#[test]
+fn runs(){
+    let mut cmd = Command::cargo_bin("recho").unwrap();
+    cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello(){
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("recho").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
+}
