96dfb184f56db72834d3ad33697a807099923351
ahmad <ahmadsaeed3290@gmail.com>
2026-02-20 00:07:23 +0500

    Feat: Add arg for text(required) and omit newline

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("recho")
+    let matches = App::new("recho")
     .version("0.1.0")
     .author("Ahmad Saeed <ahmadsaeed3290@gmail.com>")
     .about("Rust echo")
+    .arg(
+        Arg::with_name("text")
+        .value_name("TEXT")
+        .help("Input text")
+        .required(true)
+        .min_values(1),
+    )
+    .arg(
+        Arg::with_name("omit_newline")
+        .short("n")
+        .help("Don't print newline")
+        .takes_value(false),
+    )
     .get_matches();
+
+    println!("{:?}", matches);
 }
 
